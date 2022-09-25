9166fe4d6bbfef94cffcab4259ee461dcfc8f101
ben lepsch <blepsch@gmail.com>
2022-09-25 19:19:46 -0400

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# heroku stuff
+try:
+	if 'HEROKU' in os.environ:
+		import django_heroku
+		django_heroku.settings(locals())
+except ImportError:
+	found = False
