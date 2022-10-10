db439b4ca87275200d60ab2850f4ebd6c60bc2e2
kevingrieco <kcg9ue@virginia.edu>
2022-10-10 04:08:20 -0400

    Fix again

--- oauth_app/apps.py
+++ oauth_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LoginConfig(AppConfig):
+class OauthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'login'
+    name = 'oauth_app'
