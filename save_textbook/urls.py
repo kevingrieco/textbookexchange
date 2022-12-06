f246b66f099c4d7ccfa7727b5123d2022cc94ffb
alamo2000 <alexmorris0927@gmail.com>
2022-12-06 17:36:49 -0500

    save button removal

--- save_textbook/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'save_textbook'
-
-urlpatterns = [
-    path('<int:pk>/save_confirm/', views.save_confirm, name='save_confirm'),
-    path('<int:pk>/save/', views.save_textbook, name='save_textbook')
-    ]
