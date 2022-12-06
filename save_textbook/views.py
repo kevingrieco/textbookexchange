92a840cce511ccc0afb9fdd7bf431d06a1fe362f
alamo2000 <alexmorris0927@gmail.com>
2022-12-06 02:23:28 -0500

    Save url links

--- save_textbook/views.py
+++ save_textbook/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, redirect
 from post_textbook.models import Department, Course, Textbook
 
 # Create your views here.
+def save_confirm(request, pk):
+    textbook = Textbook.objects.get(pk=pk)
+    view = request.POST.get('view')
+    if request.user == textbook.user:
+        return render(request, 'save_textbook/save_confirm.html', {'textbook':textbook, 'view':view})
+    else:
+        return redirect(view)
 
 def save_textbook(request, pk):
     textbook = Textbook.objects.get(pk=pk)
