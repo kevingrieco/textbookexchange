2872c6f1a4cf31e2ac15f0e433acdd40f525c56b
alamo2000 <alexmorris0927@gmail.com>
2022-12-06 01:48:42 -0500

    error with url

--- save_textbook/urls.py
+++ save_textbook/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'remove_textbook'
+app_name = 'save_textbook'
 
 urlpatterns = [
-    path('<int:pk>/delete/', views.save_textbook, name='save_textbook'),
+    path('<int:pk>/save/', views.save_textbook, name='save_textbook'),
     ]
