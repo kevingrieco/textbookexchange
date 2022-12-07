c8ef48d8dd9ab57f28d05d64285ecdaad0defa3d
kevingrieco <kcg9ue@virginia.edu>
2022-12-06 19:10:40 -0500

    Nevermind

--- oauth_app/tests.py
+++ oauth_app/tests.py
@@ -6,11 +6,11 @@ import requests
 
 # Create your tests here.
 
-# class GoogleLoginTestCases(TestCase):
+class GoogleLoginTestCases(TestCase):
 
-#     def test_login_successful(self):
-#         req = requests.get("https://uva-cs3240-b-24.herokuapp.com/")
-#         self.assertEqual(req.status_code, 200)
+    def test_login_successful(self):
+        req = requests.get("https://uva-cs3240-b-24.herokuapp.com/")
+        self.assertEqual(req.status_code, 200)
 
    
 class SearchTestCases(TestCase):     
